376b6924ebb0bf1ca7afd5efd7b2f02616c8e1f0
Ben <ruubixo@gmail.com>
2019-03-15 19:20:20 -0400

    Spellchecked and refactored views and urls.

--- BensBlog/views.py
+++ BensBlog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpReponse
+from django.http import HttpResponse
+
 
 def home(request):
-    return HttpReponse('<h1>Blog Home</h1>')
+    return HttpResponse('<h1>Blog Home</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
