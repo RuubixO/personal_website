58e616ae0425d397cf21d2fe5ebbc102f29f6017
Ben <ruubixo@gmail.com>
2019-03-18 21:48:30 -0400

    Established test_main_pages_return.py file in "tests" directory.

---
+++ tests/test_main_pages_return_successfully.py
@@ -0,0 +1,17 @@
+"""I want a test that can cycle over each page of my Django website and confirm
+the existance of each web page"""
+
+# psuedo
+expected_page_urls = [
+                    '/home/',
+                    '/BensBlog/',
+                    '/aboutme/',
+                    '/resume/',
+                    '/contactme/'
+                    ]
+
+# psuedo
+"""for pageurl in expected_page_urls:
+    var result = requestPage(pageurl)
+    assertPageSuccess(result)
+    """
