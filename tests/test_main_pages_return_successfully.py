f9994d890b99e40b7436588ebbe99babfbf581a5
Ben's laptop <ruubixo@gmail.com>
2019-03-22 18:41:01 -0400

    Remove pseudo code and substituted response.get http pytest test.

--- tests/test_main_pages_return_successfully.py
+++ tests/test_main_pages_return_successfully.py
@@ -1,8 +1,12 @@
 """I want a test that can cycle over each page of my Django website and confirm
 the existance of each web page"""
 
-# psuedo
+from django.test import Client, TestCase
+
+
+# check pages and return http status code
 expected_page_urls = [
+                    '/admin/',
                     '/home/',
                     '/BensBlog/',
                     '/aboutme/',
@@ -10,8 +14,21 @@ expected_page_urls = [
                     '/contactme/'
                     ]
 
-# psuedo
-"""for pageurl in expected_page_urls:
-    var result = requestPage(pageurl)
-    assertPageSuccess(result)
-    """
+
+# test that each page comes back correctly
+def test_page_success():
+    for pageurl in expected_page_urls:
+        c = Client()
+        response = c.get(pageurl)
+
+        print(pageurl)
+
+        assert response.status_code == 200
+
+
+# test to dig deeper into redirect issues with '/admin/'
+def test_admin_redirects_page():
+    c = Client()
+    response = c.get('/admin/')
+
+    assert response.status_code == 302
