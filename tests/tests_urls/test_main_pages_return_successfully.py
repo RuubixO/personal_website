48f80796d1b7e6bc73dde8abc81ff5dfe45d9bab
Ben's laptop <ruubixo@gmail.com>
2019-04-05 19:57:55 -0400

    Reorganized test folder to bundle unit tests by type (urls.py, views.py, etc)

---
+++ tests/tests_urls/test_main_pages_return_successfully.py
@@ -0,0 +1,45 @@
+"""I want a test that can cycle over each page of my Django website and confirm
+the existance of each web page"""
+
+from django.test import Client, TestCase
+import pytest
+
+
+# check pages and return http status code
+expected_page_urls = [
+                    '/admin/',
+                    '/home/',
+                    '/BensBlog/',
+                    '/aboutme/',
+                    '/resume/',
+                    '/contactme/',
+                    '/dummy/'
+                    ]
+
+
+# # test that each page comes back correctly
+# @pytest.mark.parametrize('test_urls', expected_page_urls)
+# def test_page_success(client, test_urls):
+#     print(client)
+#     response = client.get(test_urls)
+#
+#     print(test_urls)
+#
+#     assert response.status_code == 200 or 302
+
+# revised test (above) that uses Client()
+@pytest.mark.parametrize('test_urls', expected_page_urls)
+def test_page_success(test_urls):
+    c = Client()
+    response = c.get(test_urls)
+
+    print(test_urls)
+
+    assert response.status_code == 200 or response.status_code == 302
+
+# test to dig deeper into redirect issues with '/admin/'
+def test_admin_redirects_page():
+    c = Client()
+    response = c.get('/BensBlog/')
+
+    assert response.status_code == 200
