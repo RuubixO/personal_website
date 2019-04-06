34cebdfebd649e25c81f5c932c308e232aa4a3bd
Ben <ruubixo@gmail.com>
2019-04-05 20:00:14 -0400

    Configured project/settings.py to use the python-dotenv third party module for .env files.

--- MyDjango2Site/settings.py
+++ MyDjango2Site/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get()
+SECRET_KEY = os.getenv('SECRET_KEY_TEST')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
