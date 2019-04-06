ccb9f9eb8d7c1d28c0b01e5ec63359a07d9e9f44
Ben <ruubixo@gmail.com>
2019-04-05 22:09:14 -0400

    Removed extra http 200 assert function.

--- tests/tests_urls/test_main_pages_return_successfully.py
+++ tests/tests_urls/test_main_pages_return_successfully.py
@@ -1,7 +1,7 @@
 """I want a test that can cycle over each page of my Django website and confirm
 the existance of each web page"""
 
-from django.test import Client, TestCase
+# from django.test import Client, TestCase
 import pytest
 
 
@@ -17,21 +17,11 @@ expected_page_urls = [
                     ]
 
 
-# # test that each page comes back correctly
-# @pytest.mark.parametrize('test_urls', expected_page_urls)
-# def test_page_success(client, test_urls):
-#     print(client)
-#     response = client.get(test_urls)
-#
-#     print(test_urls)
-#
-#     assert response.status_code == 200 or 302
-
-# revised test (above) that uses Client()
+# test that each page comes back correctly; specifically pytest-django plugin
 @pytest.mark.parametrize('test_urls', expected_page_urls)
-def test_page_success(test_urls):
-    c = Client()
-    response = c.get(test_urls)
+def test_page_success(client, test_urls):
+
+    response = client.get(test_urls)
 
     print(test_urls)
 
