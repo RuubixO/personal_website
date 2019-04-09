e9c0e7b917b643ee1c6155e8cb0afb7fa94d1a43
Ben's laptop <ruubixo@gmail.com>
2019-04-09 10:40:32 -0400

    Added new test files for django models and views.

---
+++ tests/tests_models/test_new_blog_entry.py
@@ -0,0 +1,16 @@
+"""
+I am an authorized user/author that can:
+1. log into the website
+2. add a blog post with
+    a. author name
+    b. post title
+    c. post date
+        c1. ability to update post if needed
+    d. post's content:
+        d1. text
+        d2. image
+        d3. hyperlinks
+        d4. videos
+3. add to postgres database.
+4. call up postgres database with all the above details
+"""
